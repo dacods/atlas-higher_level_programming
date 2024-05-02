0a81d6b2457ac7aa775af97cc6dd448126c2916c
dacods <135403583+dacods@users.noreply.github.com>
2024-05-02 18:30:38 -0500

    Fixed 1-last_digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 print(f"Last digit of {number} is {number % 10} ", end='')    
 if number % 10 > 5:
     print("and is greater than 5")
-elif abs(number) % 10 == 0:
+elif number % 10 == 0:
     print("and is 0")
-elif number % 10 < 6:
+elif abs(number) % 10 < 6:
     print("and is less than 6 and not 0")
