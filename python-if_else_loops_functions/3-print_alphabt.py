c0f88e00b04a50e82436a2fbbd032bdcd8d65c41
dacods <135403583+dacods@users.noreply.github.com>
2024-05-02 19:35:14 -0500

    Fixed 3-print_alphabet

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-for ascii in range(ord('a'), ord('z')+1):
-    letters = chr(ascii)
-    if letters != 'e' and letters != 'q':
-        print(letters, end='')
+print(''.join([chr(i) for i in range(97, 123) if chr(i) not in ['e', 'q']]).format(), end="")
