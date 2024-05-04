fa3d02d974a951b025100f3045e7ed7d26fed827
dacods <135403583+dacods@users.noreply.github.com>
2024-05-04 12:17:11 -0500

    Fixed 8-uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def uppercase(s):
-    for char in s:
-        uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char 
-        print(uppercase_char, end="")
-    print()
+def uppercase(str):
+    if 97 <= ord(char) <= 122:
+        char = chr(ord(char) - 32)
+    print("{}".format(char), end="")
+    print("")
