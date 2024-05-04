3b8fb1682c9f1169c02ea360e3431b5f92f5d939
dacods <135403583+dacods@users.noreply.github.com>
2024-05-04 12:03:38 -0500

    Created 6-print_comb3

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:02d}".format(i), 
+              "{:02d}".format(j), sep=", ", 
+              end=", " if i != 8 or j != 9 else "\n")
