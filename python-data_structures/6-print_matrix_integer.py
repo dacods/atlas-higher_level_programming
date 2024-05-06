c68d80b869064879b9fd9d853cdbda333c5ee8db
dacods <135403583+dacods@users.noreply.github.com>
2024-05-06 06:40:52 -0500

    Fixed 6-print_matrix_integer

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:4d}".format(num), end=" ")
+            print("{:d}".format(num), end=" ")
         print()
