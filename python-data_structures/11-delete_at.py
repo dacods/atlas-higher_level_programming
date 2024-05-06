3a44ff28e64c0965257e93418635ca1942ea1bc4
dacods <135403583+dacods@users.noreply.github.com>
2024-05-06 16:03:54 -0500

    Fixed 11-delete_at

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,10 +4,7 @@ def delete_at(my_list=[], idx=0):
         my_list = []
 
 
-    if idx < 0 or idx >= len(my_list):
+    if idx > 0 or idx <= len(my_list):
         return my_list
-    
-    if idx == len(my_list) - 1:
-        return my_list[:idx]
     
     return my_list[:idx] + my_list[idx+1:]
