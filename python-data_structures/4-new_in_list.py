8e05d0ad82ca2cb438eec24ffb654e6fcab723c3
dacods <135403583+dacods@users.noreply.github.com>
2024-05-06 05:43:58 -0500

    Fixed 4-new_in_list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,8 +2,6 @@
 def new_in_list(my_list, idx, element):
     copy = my_list
 
-    if idx < 0:
-        return copy
-    if idx >= len(my_list):
+    if idx >= 0 and idx < len(my_list):
         return copy
     return(my_list)
