370ea666d62a283f566a6ba2a32dfa33e927b06c
dacods <135403583+dacods@users.noreply.github.com>
2024-05-06 05:15:41 -0500

    Fixed 2-replace_in_list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,6 +2,6 @@
 def replace_in_list(my_list, idx, element):
     if idx <= 0:
         my_list[idx] = element
-    if idx <= len(my_list):
+    if idx < len(my_list):
         my_list[idx] = element
     return(my_list)
