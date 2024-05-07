827b323a15c45106d715223c7785ad275bfd0926
dacods <135403583+dacods@users.noreply.github.com>
2024-05-07 17:54:02 -0500

    Fixed 4-list_division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     try:
-        for i in range(list_length):
+        for i in range(0, list_length):
             try:
                 element_1 = my_list_1[i] if i < len(my_list_1) else 0
                 element_2 = my_list_2[i] if i  < len(my_list_2) else 0
