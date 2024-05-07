26547fd1dabc4282ad2cd776650905ea7b457158
dacods <135403583+dacods@users.noreply.github.com>
2024-05-07 17:55:58 -0500

    Fixed 4-list_division

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     try:
-        for i in range(0, list_length):
+        for i in range(list_length):
             try:
                 element_1 = my_list_1[i] if i < len(my_list_1) else 0
                 element_2 = my_list_2[i] if i  < len(my_list_2) else 0
@@ -11,11 +11,11 @@ def list_division(my_list_1, my_list_2, list_length):
                 if element_2 == 0:
                     raise ZeroDivisionError("division by 0")
                 result.append(element_1 / element_2)
-            except ZeroDivisionError as e:
-                print(e)
+            except ZeroDivisionError:
+                print("division by 0")
                 result.append(0)
-            except TypeError as e:
-                print("e")
+            except TypeError:
+                print("wrong type")
                 result.append(0)
             except IndexError:
                 print("out of range")
