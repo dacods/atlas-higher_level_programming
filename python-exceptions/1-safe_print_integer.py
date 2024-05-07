19f1be5bf4d139ff7835022d5471e29223c1e51d
dacods <135403583+dacods@users.noreply.github.com>
2024-05-06 20:01:41 -0500

    Fixed 1-safe_print_integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
-        return isinstance(value, int)
     except (ValueError, TypeError):
         return False
     return True
