a5cd39005c65e9b49de1f94281cfe519d071631d
dacods <135403583+dacods@users.noreply.github.com>
2024-05-11 18:15:50 -0500

    Create 4-square

---
+++ python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define a class"""
+
+
+class Sqaure:
+    """Defines a Square"""
+    
+    def __init__(self, size=0):
+        self.__size - size
+
+    def size(self):
+        return self.size
+
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
