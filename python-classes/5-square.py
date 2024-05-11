bf5715cfdd3e670295442413bd418bda62338847
dacods <135403583+dacods@users.noreply.github.com>
2024-05-11 18:53:39 -0500

    Fixed 5-square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -10,7 +10,7 @@ class Square:
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -19,10 +19,10 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-        
+
     def area(self):
         return self.__size ** 2
-    
+
     def my_print(self):
         if self.__size == 0:
             print()
