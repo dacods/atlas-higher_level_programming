11d4acca6df83b5aaff81193b1013434c7ef0090
dacods <135403583+dacods@users.noreply.github.com>
2024-05-11 18:26:32 -0500

    Fixed 4-square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,9 +14,10 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     def area(self):
         return self.__size ** 2
