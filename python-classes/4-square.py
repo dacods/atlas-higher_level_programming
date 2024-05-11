8711aca1185fe7613a646aa932214312cb9a81c6
dacods <135403583+dacods@users.noreply.github.com>
2024-05-11 18:17:23 -0500

    Fixed documentation in  4-square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Define a class"""
+"""Defines a class"""
 
 
 class Sqaure:
     """Defines a Square"""
-    
+
     def __init__(self, size=0):
         self.__size - size
 
