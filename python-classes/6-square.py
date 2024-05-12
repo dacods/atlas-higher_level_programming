8edea1372d5d2235b2dc63070edafb2decf52375
dacods <135403583+dacods@users.noreply.github.com>
2024-05-11 19:27:34 -0500

    Created 6-square

---
+++ python-classes/6-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Defines a class"""
+
+
+class Square:
+    """Defines a Square"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+    
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    
+    def position(self):
+        return self.__position
+    
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2 or \
+        not all(isinstance(i, int) for i in value) or \
+        not all(i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive intgers")
+        self.__position = value
+        
+    def area(self):
+        return self.__size ** 2
+    
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        
+        for _ in range(self.__position[1]):
+            print()
+
+        for _ in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
