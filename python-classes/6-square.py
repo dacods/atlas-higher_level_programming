460ce17ec91d3b4ec1e91d329c8e585c8e42f0d8
dacods <135403583+dacods@users.noreply.github.com>
2024-05-11 19:32:26 -0500

    Fixed 6-square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -11,7 +11,7 @@ class Square:
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -20,11 +20,11 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
     @property
     def position(self):
         return self.__position
-    
+
     @position.setter
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2 or \
@@ -32,15 +32,15 @@ class Square:
         not all(i >= 0 for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-        
+
     def area(self):
         return self.__size ** 2
-    
+
     def my_print(self):
         if self.__size == 0:
             print()
             return
-        
+
         for _ in range(self.__position[1]):
             print()
 
