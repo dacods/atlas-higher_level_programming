110bf82685af28a6e7acc6770e520d0364a8c6cf
dacods <135403583+dacods@users.noreply.github.com>
2024-05-14 17:19:53 -0500

    Made changes in 1-rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -16,8 +16,10 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
+        else:
+            self.width = value
         
     @property
     def height(self):
@@ -27,5 +29,7 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
+        else:
+            self.height = value
