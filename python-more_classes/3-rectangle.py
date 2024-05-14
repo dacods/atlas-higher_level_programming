8f36207349f24c7ab83727554c54d28ae892cbc5
dacods <135403583+dacods@users.noreply.github.com>
2024-05-14 17:53:44 -0500

    Made changes in 3-rectangle

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -43,7 +43,7 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             return ""
         
         rect = ""
