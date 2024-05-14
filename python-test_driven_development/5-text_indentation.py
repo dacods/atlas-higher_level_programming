f92381ca67bde54e1f1b9bab165c6ab71a9da0ca
dacods <135403583+dacods@users.noreply.github.com>
2024-05-14 02:55:40 -0500

    Created 5-text_indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Function that prints a text with 2 new lines"""
+
+
+def text_indentation(text):
+    """Prints new lines after ., /, and :"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    punctuations = ['.', '?', ':']
+    lines = []
+    line = ''
+
+    for char in text:
+        line += char
+        if char in punctuations:
+            lines.append(line.strip())
+            lines.append('')
+            line = ''
+
+    if line:
+        lines.append(line.strip())
+
+    for line in lines:
+        print(line)
