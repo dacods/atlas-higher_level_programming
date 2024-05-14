14ef7faafd62c808dbbbfd799dff47261cc54d60
dacods <135403583+dacods@users.noreply.github.com>
2024-05-14 02:04:37 -0500

    Made changes in 2-matrix_divided

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,25 +6,25 @@ def matrix_divided(matrix, div):
     """Retruns new_matrix or raises and error"""
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+
     for row in matrix:
         if not all(isinstance(elem, (int, float)) for elem in row):
             raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        
+
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
             raise TypeError("Each row of the matrix must have the same size")
-        
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     new_matrix = []
     for row in matrix:
         new_row = [round(elem / div, 2) for elem in row]
         new_matrix.append(new_row)
-    
+
     return new_matrix
