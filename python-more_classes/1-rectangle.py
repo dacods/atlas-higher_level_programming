53e9abab70f5906bdb30a1fe7a8b57152425c091
dacods <135403583+dacods@users.noreply.github.com>
2024-05-14 17:24:54 -0500

    Made changes in 1-rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,12 +5,12 @@
 class Rectangle:
     """Defines a rectangle"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        return self.__width
+        return self.width
 
     @width.setter
     def width(self, value):
@@ -19,11 +19,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         else:
-            self.__width = value
+            self.width = value
 
     @property
     def height(self):
-        return self.__height
+        return self.height
 
     @height.setter
     def height(self, value):
@@ -32,4 +32,4 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         else:
-            self.__height = value
+            self.height = value
