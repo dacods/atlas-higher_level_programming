5026ebd0390824ce441f191ed94b9b1dca366306
dacods <135403583+dacods@users.noreply.github.com>
2024-05-14 03:34:41 -0500

    Created 6-max_integer

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""max integer test"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class MaxTest(unittest.TestCase):
+    """Testing"""
+    def testing_max_at_end(self):
+        """Testing at end"""
+        self.asserEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def testing_max_at_beginning(self):
+        """"Testing at beginning"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def testing_max_at_middle(self):
+        """Testing at middle"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def testing_with_one_int(self):
+        """Testing with one element"""
+        self.assertEqual(max_integer([1]), 1)
+
+    def testing_with_the_same_int(self):
+        """Testing with the same int"""
+        self.assertEqual(max_integer([4, 4, 4, 4]), 4)
+    
+    def testing_with_none(self):
+        """Testing with nothing"""
+        self.assertEqual(max_integer([]), None)
+    
+    def testing_with_negative(self):
+        """Testing with negatives"""
+        self.assertEqual(max_integer([-1, -4, -3, -2]), -4)
