2bce523499488cb4259d42e03c4ba8953a613584
dacods <135403583+dacods@users.noreply.github.com>
2024-05-14 19:41:15 -0500

    Made changes in 9-rectangle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -64,6 +64,7 @@ class Rectangle():
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
@@ -73,5 +74,6 @@ class Rectangle():
             return rect_1
         return rect_2
 
+    @classmethod
     def square(cls, size=0):
         return cls(size, size)
