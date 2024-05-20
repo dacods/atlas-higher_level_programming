2d6af019ca278992ee49f97d2cebe9f864c4ef4a
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 03:17:29 -0500

    Made changes in 3-is_kind_of_class

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,6 +3,6 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """Return true if object is an instance of the 
+    """Return true if object is an instance of the
     object or class otherwise flase"""
     return isinstance(obj, a_class)
