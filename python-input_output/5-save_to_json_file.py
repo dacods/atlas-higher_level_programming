5e7a2fb388c9f529d673c9cd33db8c791454ff50
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 14:50:33 -0500

    Made changes in 5-save_to_json_file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 def save_to_json_file(my_obj, filename):
     """Writes an object to a text file using a JSON representation"""
     with open(filename, 'w') as file:
-        value = {my_obj}
+        value = [my_obj]
 
-        return json.dumps(value)
+    return json.dumps(value)
