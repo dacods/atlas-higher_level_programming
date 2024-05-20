ef6168baeec996c9bb01dff8652919aaa247a4d5
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 16:49:02 -0500

    Created 10-student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a module"""
+
+
+class Student:
+    """Defines a class"""
+    def __init__(self, firt_name, last_name, age):
+        self.first_name = firt_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                new_dict[key] = value
+        return new_dict
