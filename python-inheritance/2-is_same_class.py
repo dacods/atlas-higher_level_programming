8cd93f13d16e19e3e287aeec4fffdc95d1f11098
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 03:09:10 -0500

    Created 2-is_same_class

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """Function that returns true if exactlay an instace otherwise flase"""
-    if obj is a_class:
+    if type(obj) is a_class:
         return True
     else:
         return False
