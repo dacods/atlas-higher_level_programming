055224f936f6b84a28e3d0d2daa51d9d04b33ac2
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 04:18:31 -0500

    Created 8-rectangle

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a module"""
+
+
+class BaseGeometry:
+    """Defines a class"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
