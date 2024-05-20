38792ab33aa3693d7814c558edd93108fb4d492a
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 14:26:11 -0500

    Made changes in 3-to_json_string

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -5,5 +5,5 @@ import json
 
 def to_json_string(my_obj):
     """Returns the JSON representation of an object"""
-    data = json.dumps(my_obj, indent=4)
+    data = json.dumps(my_obj)
     return data
