12749cf4166d1361576deec749a0ea593180eb9a
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 15:21:41 -0500

    Made changes in 6-load_from_json_file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -6,7 +6,7 @@ import json
 def load_from_json_file(filename):
     """Creates an object from a JSON file"""
     try:
-        with open(filename, mode='x') as file:
+        with open(filename, 'x') as file:
             file.write(file)
     except FileExistsError:
         pass
