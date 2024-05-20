4e6a52528d29f50b528174cf46b0513d5b443d95
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 13:13:17 -0500

    Made changes in 1-write_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,10 +5,5 @@
 def write_file(filename="", text=""):
     """Function that writes a string to a text file and
     returns the number of characters written"""
-    count = 0
-    with open(filename) as f:
-        for line in f:
-            for char in line:
-                if char in text:
-                    count += 1
-    return count
+    with open(filename, 'w') as text:
+        text.write("{}".format(text))
