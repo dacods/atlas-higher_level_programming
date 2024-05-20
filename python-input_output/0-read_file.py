019b97e4ebeab9ec7f66247202c1f948dcf3ea05
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 12:43:09 -0500

    Created README.md and 0-read_file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a module"""
+
+
+def read_file(filename=""):
+    """Function that reads a text file and prints to stdout"""
+    with open(filename, 'r') as fin:
+        print(fin.read())
