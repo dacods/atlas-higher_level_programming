e720d6cd84a93b037a9189799eea8e0d5ad868fc
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 15:12:18 -0500

    Made changes in 6-load_from_json_file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,8 +5,6 @@ import json
 
 def load_from_json_file(filename):
     """Creates an object from a JSON file"""
-    data = {"{}".format(filename)}
-
-    x = json.loads(data)
-
+    with open(filename, 'w+') as file:
+        x = json.loads(file)
     return x
