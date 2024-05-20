f4a813f09d116c22a41922af3faa3d53c247dd8b
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 04:24:15 -0500

    Made changes in 8-rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """Defines a module"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Defines a class"""
     def __init__(self, width, height):
