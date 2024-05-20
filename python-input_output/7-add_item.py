cb412dc35945d52496827971e697df98f407bdce
dacods <135403583+dacods@users.noreply.github.com>
2024-05-20 16:37:51 -0500

    Created 7-add_item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
 """Defines a module"""
-import json
-save_to_
+import sys
+import os
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
