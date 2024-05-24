b42030bc51ca45282ab940cd13d275b051b8161a
dacods <135403583+dacods@users.noreply.github.com>
2024-05-24 16:03:55 -0500

    Made changes in rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,15 +14,15 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, width):
         self.__width = width
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, height):
         self.__height = height
@@ -30,7 +30,7 @@ class Rectangle(Base):
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, x):
         self.__x = x
@@ -38,11 +38,11 @@ class Rectangle(Base):
     @property
     def y(self):
         return self.__y
-    
+
     @y.setter
     def y(self, y):
         self.__y = y
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
