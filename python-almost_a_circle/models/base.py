a9a592f81792207b965cf25a9527a916596d3452
dacods <135403583+dacods@users.noreply.github.com>
2024-05-27 16:26:20 -0500

    Made changes in base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,4 +20,4 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         else:
-            json.dumps([list_dictionaries])
+            return json.dumps(list_dictionaries)
