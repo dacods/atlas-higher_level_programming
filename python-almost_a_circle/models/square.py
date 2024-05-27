abb9b09218e5c3036d0c4c2e9d02b5fae92bf127
dacods <135403583+dacods@users.noreply.github.com>
2024-05-27 13:53:52 -0500

    Made changes in square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,7 +6,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Defines a class"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, size, size)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return (f"[Square] ({self.id}) {self.__x}/{self.__y} - "
