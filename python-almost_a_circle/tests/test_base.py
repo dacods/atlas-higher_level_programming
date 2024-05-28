5b78a5b1ea878c02ab3f893258bb270ce847a52d
dacods <135403583+dacods@users.noreply.github.com>
2024-05-28 14:09:11 -0500

    Created test_base

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,13 @@
+import json, os, unittest, sys
+from models.base import Base
+from models.rectangle import Rectangle
+"""unit test for base"""
+
+
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        Base.__Base__nb_objects = 0
+
+    def test_init(self):
+        base_class = Base()
+        self.assertEqual(base_class.id, 1)
