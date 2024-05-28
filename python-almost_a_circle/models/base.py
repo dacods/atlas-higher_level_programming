0ac861164ddd398b408bbb836c6cb911180cffa3
dacods <135403583+dacods@users.noreply.github.com>
2024-05-28 13:33:38 -0500

    Made changes in base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,3 +38,15 @@ class Base:
         if json_string is None:
             return []
         return json.loads(json_string)
+    
+    def create(cls, **dictionary):
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        else:
+            dummy == cls()
+
+        dummy.update(**dictionary)
+
+        return dummy
