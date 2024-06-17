d8bdd7e74722143d86c15ba10578d3f2731d384a
dacods <135403583+dacods@users.noreply.github.com>
2024-06-17 15:23:24 -0500

    Made changes in 9-full_creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Creates a table in the database in MySQL server and adds multiple rows
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256)
     score INT
