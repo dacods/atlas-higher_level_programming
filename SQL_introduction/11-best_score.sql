68b49c562e83b72c26cfd897ba5661113b35fcaa
dacods <135403583+dacods@users.noreply.github.com>
2024-06-17 15:50:56 -0500

    Fixed typo in 11-best_score

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- List all records greater than or equal to 10 from a table in a database in MySQL server
-SELECT score, name FROM second_table WHERE scpre >= 10
+SELECT score, name FROM second_table WHERE score >= 10
 ORDER BY score DESC;
