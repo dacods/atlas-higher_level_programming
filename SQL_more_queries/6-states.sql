3e8d4f7c654df623451598f42c6f82a0df04dd02
dacods <135403583+dacods@users.noreply.github.com>
2024-06-18 12:53:11 -0500

    Made changes in' 6-states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Creates a database and a table in MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT UNIQUE AUTO PRIMARY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256)
 )
