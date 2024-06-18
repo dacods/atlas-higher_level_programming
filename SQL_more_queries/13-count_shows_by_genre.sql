c5fbbaa8eeee6901fe98c86c8385c102dc829328
dacods <135403583+dacods@users.noreply.github.com>
2024-06-18 14:37:16 -0500

    Made changes in 13-count_shows_by_genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,6 @@
 SELECT tv_show_genres.genre_id AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
 JOIN tv_shows
-ON tv_show_genres.id = tv_shows.id
+ON tv_show_genres.show_id = tv_shows.id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
