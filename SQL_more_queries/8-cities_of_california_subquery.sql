9f1485c09774c1c17e1881966316cbe91acc13ba
dacods <135403583+dacods@users.noreply.github.com>
2024-06-18 13:33:02 -0500

    Made changes in 8-cities_of_california

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- List all of the cities of California that can be found in the database
-SELECT @california_id := id FROM states WHERE name = 'California';
+SET @california_id = (SELECT id FROM states WHERE name = 'California');
 
 SELECT id, name FROM cities WHERE state_id = @california_id ORDER BY id ASC;
