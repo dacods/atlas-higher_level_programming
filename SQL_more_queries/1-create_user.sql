a13adc0bf89de0eb80f602f40fddddb133ba7f87
dacods <135403583+dacods@users.noreply.github.com>
2024-06-18 10:07:03 -0500

    Made changes in 1-create_user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates MySQL server user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON my_db.* TO 'user_0d_1'@'localhost';
+GRANT ALL ON "." TO 'user_0d_1'@'localhost';
