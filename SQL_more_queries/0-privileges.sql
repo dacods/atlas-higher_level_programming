72d4d2d12dcee10c68874fd32c23fdae5f90ea94
dacods <135403583+dacods@users.noreply.github.com>
2024-06-18 09:54:23 -0500

    Made changes in 0-privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- List all privileges of MySQL users
-SHOW GRANTS FOR 'user_0d_1'@'loaclhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- List all privileges of MySQL users
-SHOW GRANTS FOR 'user_0d_2'@'loaclhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
