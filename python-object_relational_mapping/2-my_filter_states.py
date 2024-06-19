a42e6d70dde8a4934c88840766b49982c2f7e394
dacods <135403583+dacods@users.noreply.github.com>
2024-06-19 16:01:41 -0500

    Made changes in 2-my_filter_states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
                          password=sys.argv[2], db=sys.argv[3], port=3306)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '{}' \
+    cursor.execute("SELECT * FROM states WHERE BINARY name = '{}' \
                    ORDER BY id ASC;".format(sys.argv[4]))
 
     states = cursor.fetchall()
