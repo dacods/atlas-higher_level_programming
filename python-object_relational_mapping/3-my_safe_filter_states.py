a822792ca64dd29ab37b1713e84adfa1e09930f9
dacods <135403583+dacods@users.noreply.github.com>
2024-06-19 16:17:56 -0500

    Made changes in 3-my_safe_filter_states

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
                          password=sys.argv[2], db=sys.argv[3], port=3306)
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = '%s'".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name = %s;".format(sys.argv[4]))
 
     states = cursor.fetchall()
 
