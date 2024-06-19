a824aa43be03f06af0d87d60cfa3d7a5bfe9b6eb
dacods <135403583+dacods@users.noreply.github.com>
2024-06-19 17:02:31 -0500

    Made changes in model_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,9 +3,8 @@
 and an instance Base = declarative_base()"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
-
 Base = declarative_base()
+
 
 class State(Base):
     """Defines a class"""
