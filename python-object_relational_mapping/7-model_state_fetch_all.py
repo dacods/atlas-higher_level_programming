8fb2dd7ebe3252de0db5b19d17ba51320466a46b
dacods <135403583+dacods@users.noreply.github.com>
2024-06-19 17:11:57 -0500

    Created 7-model_state_fetch_all

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""lists all State objects from the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy import sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{user}:{password}@localhost:3306/{db}')
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session
+
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"({state.id}: {state.name})")
+
+    session.close()
