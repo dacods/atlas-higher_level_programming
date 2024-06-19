36eddacf9d4f11d080ce4078a6a0fcc08a12d02a
dacods <135403583+dacods@users.noreply.github.com>
2024-06-19 17:52:28 -0500

    Created 12-model_state_update_id_2

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""changes the name of a State object from the database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine(
+        f'mysql+mysqldb://{user}:{password}@localhost:3306/{db}'
+        )
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state_to_update = session.query(State).filter_by(id=2).first()
+
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
