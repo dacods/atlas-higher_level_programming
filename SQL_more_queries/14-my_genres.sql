b98ac412a7cfa5f0f9d5ea2fd91d33fee9bf894d
dacods <135403583+dacods@users.noreply.github.com>
2024-06-19 14:53:32 -0500

    Made changes in 14-my_genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,4 @@ FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
 JOIN tv_genres ON tv_show_genres.tv_genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY genres.name ASC;
+ORDER BY tv_genres.name ASC;
