debe21c4541610efe299683473c6c69dc3db9ddb
dacods <135403583+dacods@users.noreply.github.com>
2024-06-19 14:36:07 -0500

    Made changes in 13-count_shows_by_genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,9 @@
 -- list all genres from database and display number of shows linked to each
-SELECT tv_show_genres.genre_id AS genre, 
+SELECT genres.name AS genre, 
     COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY tv_show_genres.genre_id
+JOIN genres ON tv_show_genres.genre_id = genres.id
+GROUP BY genres.name
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
