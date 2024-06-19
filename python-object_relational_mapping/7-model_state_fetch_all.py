2f121d732f903c586d5ee2dac3f219d6996fe993
dacods <135403583+dacods@users.noreply.github.com>
2024-06-19 17:16:41 -0500

    Made changes in 7-model_state_fetch_all

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == '__main__':
     user = sys.argv[1]
     password = sys.argv[2]
@@ -14,11 +15,9 @@ if __name__ == '__main__':
 
     Session = sessionmaker(bind=engine)
 
-    session = Session
+    session = Session()
 
     states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print(f"({state.id}: {state.name})")
-
-    session.close()
