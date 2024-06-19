64550713aba8e650da58de299eaca88154ab0b08
dacods <135403583+dacods@users.noreply.github.com>
2024-06-19 15:34:08 -0500

    Created 0-select_states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""List all states from a database"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], db=sys.argv[3], port=3306)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states;")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
