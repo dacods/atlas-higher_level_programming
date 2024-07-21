da0f93af383c4b3637f5bc55d65bf8937093f6c8
dacods <dacods.21@gmail.com>
2024-07-21 13:48:27 -0500

    Created 9-add

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+const firstarg = Number(args[2]);
+const secondarg = Number(args[3]);
+
+function add (a, b) {
+    console.log(a + b);
+}
+
+add(firstarg, secondarg)
