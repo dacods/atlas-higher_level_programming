d3a2bfdb83406339d2a46f4b825919b8273b6d58
dacods <dacods.21@gmail.com>
2024-07-21 14:26:15 -0500

    Created 10-factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const args = process.argv;
+
+const n = parseInt(args[2]);
+
+function factorial (n) {
+  if (n === 0 || isNaN(n)) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(n));
