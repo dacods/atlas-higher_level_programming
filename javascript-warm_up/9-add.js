ba30687bf448c8d1e0df37945fd358602e2cbdf8
dacods <dacods.21@gmail.com>
2024-07-21 14:12:09 -0500

    Made changes in 9-add

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,7 +4,7 @@ const firstarg = Number(args[2]);
 const secondarg = Number(args[3]);
 
 function add (a, b) {
-    console.log(a + b);
+  console.log(a + b);
 }
 
-add(firstarg, secondarg)
+add(firstarg, secondarg);
