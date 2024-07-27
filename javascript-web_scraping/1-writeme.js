4b338fa9d8f129489369c49e722797adbc41973e
dacods <dacods.21@gmail.com>
2024-07-27 18:22:33 -0500

    Created 1-writeme

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filepath = process.argv[2];
+
+const string = process.argv[3];
+
+fs.writeFile(filepath, string, 'utf8', (err, string) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(string);
+});
