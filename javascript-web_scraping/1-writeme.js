711b077fd1b3e4931d4fae768f83844e27301148
dacods <dacods.21@gmail.com>
2024-07-27 18:30:32 -0500

    Made changes in 1-writeme

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const filepath = process.argv[2];
 
 const string = process.argv[3];
 
-fs.writeFile(filepath, string, 'utf8', (err, string) => {
+fs.writeFile(filepath, string, 'utf8', (err) => {
   if (err) {
     console.error(err);
     return;
