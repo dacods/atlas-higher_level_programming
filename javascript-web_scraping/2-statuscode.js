eea21c3195db06a8963225cb9578cb57acf2671e
dacods <dacods.21@gmail.com>
2024-07-27 19:16:08 -0500

    Made changes in 2-statuscode

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,6 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request(url, function (_, response) {
-  console.log(`code: ${response.statusCode}`);
-});
+request(url, (error, response, body) => {
+    console.log(`code: ${response.statusCode}`);
+})
