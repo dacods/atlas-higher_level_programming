14c146d1aced5e77319b7ea29a00f3ca5bb0117d
dacods <dacods.21@gmail.com>
2024-07-28 21:42:16 -0500

    Created 5-request_store

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const request = require('request');
+
+const fs = require('fs');
+
+const url = process.argv[2];
+
+const filepath = process.argv[3];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  fs.writeFile(filepath, body, 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`${filepath}`);
+  });
+});
