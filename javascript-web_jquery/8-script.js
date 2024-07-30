3415fb0548f0fd907c8f60aea91688b1b40651d0
dacods <dacods.21@gmail.com>
2024-07-30 14:50:51 -0500

    Created 8-script

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+$.ajax({
+    url: 'https://swapi-api.hbtn.io/api/films/?format=json',
+    type: 'GET',
+    success: function(data) {
+        const movies = data.results;
+        movies.forEach(function(movie) {
+            $('#list_movies').append('<li>' + movie.title + '</li>');
+        }) 
+    }
+})
